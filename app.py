85721e2ae8a37a6f80497e3842ef4d33d55d4eb0
ShayaanMujawer <shayaanmujawer@gmail.com>
2025-05-17 22:27:38 +0530

    Update all files in repo before redeployment

--- app.py
+++ app.py
@@ -210,5 +210,8 @@ def login_page():
         return redirect(url_for('dashboard'))
     return render_template('login.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
